051cdec8b7318a963df7470d546ba703f86a51fb
Christian Franchin dos Santos <christianfranchin@gmail.com>
2021-06-03 03:39:08 -0300

    Solvve issue with slash redirection
    Signed-off-by: Christian Franchin dos Santos <christianfranchin@gmail.com>

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from blueprints import plex
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.url_map.strict_slashes = False
 
 # import declared routes
 app.register_blueprint(plex.plex_app, url_prefix='/plex')
