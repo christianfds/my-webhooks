e8d401ca3d3cc1bf1985798111412a1354553430
Christian Franchin dos Santos <christianfranchin@gmail.com>
2021-06-03 19:06:05 -0300

    Sleep as android integration
    Signed-off-by: Christian Franchin dos Santos <christianfranchin@gmail.com>

---
+++ blueprints/sleep_as_android.py
@@ -0,0 +1,71 @@
+import json
+from flask import Blueprint, request
+from utils.config import Config
+
+from integration.ifttt import IFTTTHandler
+
+
+sleep_as_android_app = Blueprint('sleep_as_android_app', __name__, template_folder='templates')
+
+
+class ValidationException(BaseException):
+    pass
+
+
+class SleepAsAndroidHandler():
+    def __init__(self, user_name: str, player_uuid: str) -> None:
+        if (user_name != Config.user_name):
+            raise ValidationException('Doesn\'t run for unspecified users')
+
+        if (player_uuid != Config.player_uuid):
+            raise ValidationException('Doesn\'t run for unspecified players')
+
+        self.event_handler = IFTTTHandler()
+
+    def _lights_low_dim(self):
+        print('Ligando as luzes com brilho baixo')
+        self.event_handler.send_event('lights_low_dim')
+
+    def _lights_on(self):
+        print('Ligando as luzes')
+        self.event_handler.send_event('lights_on')
+
+    def _lights_off(self):
+        print('Desligando as luzes')
+        self.event_handler.send_event('lights_off')
+
+    def handle_event(self, event: str) -> None:
+        EVENT_MAP = {
+            'media.play': self._lights_off,
+            'media.pause': self._lights_low_dim,
+            'media.resume': self._lights_off,
+            'media.stop': self._lights_on
+        }
+
+        if event in EVENT_MAP:
+            EVENT_MAP[event]()
+
+
+@sleep_as_android_app.route('/', methods=['POST'])
+def get_webhook_entry():
+    print(request.data)
+    # print(request.data)
+    payload = request.form.to_dict().get('payload')
+    if not payload:
+        return {'success': False}, 400
+
+    payload = json.loads(payload)
+    if not payload.get('user', False):
+        return {'success': False}, 400
+
+    try:
+        user = payload.get('Account', {}).get('title')
+        player = payload.get('Player', {}).get('uuid')
+        event = payload.get('event')
+
+        handler = SleepAsAndroidHandler(user, player)
+        handler.handle_event(event)
+    except ValidationException:
+        pass
+
+    return {'success': True}, 200
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from blueprints import plex
+from blueprints import sleep_as_android
 
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.url_map.strict_slashes = False
 
 # import declared routes
 app.register_blueprint(plex.plex_app, url_prefix='/plex')
+app.register_blueprint(sleep_as_android.sleep_as_android_app, url_prefix='/sleep_as_android')
 
 app.run()
