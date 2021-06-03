dd74aaa4e83c4eb290faba65a544caf35e56c2f1
Christian Franchin dos Santos <christianfranchin@gmail.com>
2021-06-03 04:06:29 -0300

    Handle form post from plex
    Signed-off-by: Christian Franchin dos Santos <christianfranchin@gmail.com>

--- blueprints/plex.py
+++ blueprints/plex.py
@@ -7,8 +7,11 @@ plex_app = Blueprint('plex_app', __name__, template_folder='templates')
 
 @plex_app.route('/', methods=['POST'])
 def get_webhook_entry():
-    received_data = json.loads(request.data.decode('utf-8'))
-    print(received_data)
+    payload = request.form.to_dict().get('payload')
+    if payload:
+        payload = json.loads(payload)
+
+    print(payload)
 
     return make_response(jsonify({
         'teste': 'batata'
